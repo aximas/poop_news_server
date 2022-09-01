4c552a09a0d42d60248d4e0dcd0b0020e6e409ba
aximas <northdakota778@gmail.com>
2022-09-01 21:08:50 +0500

    fix: removed console logs

--- src/controllers/news.controller.ts
+++ src/controllers/news.controller.ts
@@ -6,30 +6,20 @@ import {getQueryToString, Query} from '../core/heplers/getQueryToString';
 class NewsController {
     async get(req: Request<{}, {}, {}, Query>, res: Response) {
         if (req.query) {
-            console.log('req.query', req.query);
 
             const queryString = getQueryToString(req.query);
-            console.log('queryString', queryString);
             try {
                 const news = await axios.get(`${NewsAPI.getAll}${queryString}`);
-                console.log('news', news.data);
-
                 return res.status(200).json(news.data);
-
             } catch (e: Error | any) {
-                console.log(e);
                 return res.status(e.response.status).json({...e.response.data});
             }
 
         } else {
             try {
                 const news = await axios.get(`${NewsAPI.getAll}`);
-                console.log('news', news.data);
-
                 return res.status(200).json(news.data);
-
             } catch (e: Error | any) {
-                console.log(e);
                 return res.status(e.response.status).json({...e.response.data.results});
             }
         }
