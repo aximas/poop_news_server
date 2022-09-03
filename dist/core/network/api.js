9abfb9b509b44f8b8b9f224842737932355ded31
aximas <northdakota778@gmail.com>
2022-09-04 01:51:51 +0500

    fix: some changes in package.json

--- dist/core/network/api.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NewsAPI = void 0;
-exports.NewsAPI = {
-    getAll: `${process.env.NEWS_API_URL}/news?apikey=${process.env.NEWS_API_PUB_KEY}`
-};
-//# sourceMappingURL=api.js.map
--- dist/routes/news.routes.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-//
-//
-// const newsRouter = (req, res) => {
-//
-// }
